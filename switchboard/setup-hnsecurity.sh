3c886bf02762c9007229e9463f68f53091e6dfa4
Anuraag <anuraagjain@gmail.com>
2016-02-27 01:06:35 +0000

    Setup scripts

--- switchboard/setup-hnsecurity.sh
+++ switchboard/setup-hnsecurity.sh
@@ -2,14 +2,21 @@
 ## Gen Certs
 
 
-## Copy Certs
+## Install and Run NGINX
+cd /tmp/
+wget https://varunc1961-8000.terminal.com/nginx.tgz
+sudo tar -C/ -xzvf /tmp/nginx.tgz
+wget https://raw.githubusercontent.com/terminalcloud/dotfiles/master/switchboard/nginx-code/nginx-node.conf
+sudo mv nginx-node.conf /usr/local/nginx/nginx.conf
+/usr/local/nginx/nginx
+
 
 
 ## Setup Password
-
-
-## Install and Run NGINX
-
+echo 'deb http://s3-us-west-1.amazonaws.com/cloudlabs.apt.repo/production /' | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install web-terminal
+sudo web-terminal start
 
 ## Setup IDE 
 
